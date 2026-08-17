9aaf2dc42c623544a3bdcf2271d3d0b2727b3dfa
Kona <konakona185@gmail.com>
2026-08-17 04:54:40 +0200

    Upload completo di tutti gli 11 moduli sani dal desktop

--- modules/mod-ale/src/LuaEngine/ALEDBCRegistry.cpp
+++
@@ -1,7 +0,0 @@
-#include "ALEDBCRegistry.h"
-
-std::vector<DBCDefinition> dbcRegistry = {
-    REGISTER_DBC(GemProperties, GemPropertiesEntry, sGemPropertiesStore),
-    REGISTER_DBC(Spell,         SpellEntry,         sSpellStore),
-};
-
